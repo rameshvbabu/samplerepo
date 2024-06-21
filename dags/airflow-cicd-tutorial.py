f87fcd33725091f00df91be54f57b6102f183e43
U-GLOBAL\32946 <32946@BNG0922HMKNQR3.global.ul.com>
2024-06-21 17:35:19 +0530

    Updated dag file

--- dags/airflow-cicd-tutorial.py
+++ dags/airflow-cicd-tutorial.py
@@ -3,16 +3,16 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="airflow-ci-cd-tutorial",
-    start_date=datetime(2023, 8, 15),
-    schedule="0 0 * * *",
-    tags=["tutorial", "CI/CD"]
+    dag_id="airflow-ci-cd-tutorial",
+    start_date=datetime(2023, 8, 15),
+    schedule="0 0 * * *",
+    tags=["tutorial", "CI/CD"]
 ) as dag:
-    # Tasks are represented as operators
-    task1 = BashOperator(task_id="task1", bash_command="echo task1")
-    task2 = BashOperator(task_id="task2", bash_command="echo task2")
-    task3 = BashOperator(task_id="task3", bash_command="echo task3")
-    task4 = BashOperator(task_id="task4", bash_command="echo task4")
+    # Tasks are represented as operators
+    task1 = BashOperator(task_id="task1", bash_command="echo task1")
+    task2 = BashOperator(task_id="task2", bash_command="echo task2")
+    task3 = BashOperator(task_id="task3", bash_command="echo task3")
+    task4 = BashOperator(task_id="task4", bash_command="echo task4")
 
-    # Set dependencies between tasks
-    task1 >> task2 >> task3 >> task4
+    # Set dependencies between tasks
+    task1 >> task2 >> task3 >> task4
