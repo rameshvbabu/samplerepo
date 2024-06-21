ec5fd1a9a2b0c40817c929e832ba97cb2addabb7
U-GLOBAL\32946 <32946@BNG0922HMKNQR3.global.ul.com>
2024-06-21 18:03:24 +0530

    Added sample test

---
+++ tests/test-one.py
@@ -0,0 +1,10 @@
+@pytest.fixture()
+
+def dagbag():
+    return DagBag(dag_folder="dags")
+
+def test_no_import_errors(dagbag):
+    """
+    Test Dags to contain no import errors.
+    """
+    assert not dagbag.import_errors
